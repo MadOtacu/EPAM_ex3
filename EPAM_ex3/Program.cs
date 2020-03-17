fb32032506b359db44da1eb746b5f743e9c04248
ЬфвЩефсг <pipboy1917@gmail.com>
2020-03-18 03:30:29 +0400

    Добавьте файлы проекта.

---
+++ EPAM_ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace EPAM_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = Console.ReadLine();
+            var words = str.Split(" " , StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words.Where(w => words.Count(v => v == w) == 1))
+            {
+                Console.Write(word + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
